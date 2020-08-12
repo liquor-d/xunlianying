205d1ab097b40524b11375ab742751d96360bd68
siwen <1471270281@qq.com>
2020-08-12 14:31:25 +0800

    init

--- preview_hw.py
+++ preview_hw.py
@@ -10,22 +10,20 @@ mid.tracks.append(track2)
 mid.tracks.append(track3)
 tra = [track, track2, track3]
 
-bpm = 72  #速度
+bpm = 75  #速度
+meta_time = 60 * 60 * 10 / bpm
+major_notes = [1, 2, 1, 2, 2, 1, 2, 1]   #定义调式
+base_note = 60   #C4音高
 
 def taptap(note, length, unit = track, base_num=0, delay=0, velocity=1.2, channel=0):   #单音
-   meta_time = 60 * 60 * 10 / bpm
-   major_notes = [1, 2, 1, 2, 2, 1, 2, 1]   #定义调式
-   base_note = 60
    unit.append(Message('note_on', note=base_note + base_num*12 + sum(major_notes[0:note]),\
         velocity=round(64*velocity), time=round(delay*meta_time), channel=channel))
    unit.append(Message('note_off', note=base_note + base_num*12 + sum(major_notes[0:note]),\
         velocity=round(64*velocity), time=round(meta_time*length), channel=channel))
 
 def mtaptap(yin, format, length, unit = track2, base_num= 0, velocity =1.0, channel = 2):    #多音等长
-    meta_time = 60 * 60 * 10 / bpm
-    major_notes = [1, 2, 1, 2, 2, 1, 2, 1]
-    base_note = 60
     time = round(length / len(format) * meta_time)
+
     if base_num == 0:
         base_num = [0 for i in range(len(format))]
 
@@ -36,7 +34,7 @@ def mtaptap(yin, format, length, unit = track2, base_num= 0, velocity =1.0, chan
        unit.append(Message('note_off', note=base_note + base_num[dis]*12 + sum(major_notes[0:note]),\
         velocity=round(64*velocity), time=time, channel=channel))
 
-def verse(track):      #主旋律  指定音轨
+def verse(track):     
    taptap(5, 1, track, -1)   
    mtaptap([3, 3], [0, 1], 1, track, [1, 1])  
    taptap(4, 1, track, -1)       
